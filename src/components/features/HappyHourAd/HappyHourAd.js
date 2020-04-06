ebc3792958c92e7ab78e809b5e8362f63f58a6cb
JakubOlkowicz <olkuba@poczta.fm>
2020-04-06 12:44:00 +0200

    add DaysToSummer and add formatTime to HappyHour

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './HappyHourAd.scss';
-// import formatTime from '../../../utils/formatTime';
+import { formatTime } from '../../../utils/formatTime';
 
 class HappyHourAd extends React.Component {
   constructor(){
@@ -19,12 +19,20 @@ class HappyHourAd extends React.Component {
     return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
   }
   render() {
-    const {title} = this.props;
+    const countDown = this.getCountdownTime();
+    let dynamicPromoInput;
+    if (countDown > 82800){
+      dynamicPromoInput = 'Time for reservations!!';
+    } 
+    else{
+      dynamicPromoInput = formatTime(countDown);
+    }
+    
     return(
-      <div className={styles.omponent}>
-        <h3 className={styles.title}>{title}</h3>
+      <div className={styles.component}>
+        <h3 className={styles.title}>Happy Hour</h3>
         <div className={styles.promoDescription}>
-          {this.getCountdownTime()}
+          {dynamicPromoInput}
         </div>
       </div>
     );        
